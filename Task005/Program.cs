6c0c27cd1c09c66f7857a54a0225d22159f12bd7
Yura <scsspb@mail.ru>
2022-06-24 04:36:35 +0300

    add task 006&005

--- Task005/Program.cs
+++ Task005/Program.cs
@@ -6,7 +6,7 @@
 
 Console.WriteLine("Введите 1 целое число ");
 int n = Convert.ToInt32(Console.ReadLine());
-for (int index = -n; index <= n; index++)
+for (int index = -n; index <= n; index ++)
 {
     Console.Write($"{index}, ");  
 }
