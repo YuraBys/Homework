9f4eb89069c307b4f3d8428d16e717c062f03625
Yura <scsspb@mail.ru>
2022-06-28 18:32:06 +0300

    add Task 13, 15,10

---
+++ Task013/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.WriteLine("Введите любое целое число ");
+int num = Convert.ToInt32(Console.ReadLine());
+if(num < 100)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else 
+{
+    while (num > 999)
+    {   
+       num = num/10;
+       
+    }
+    int res = num % 10;
+    Console.WriteLine(res);
+    
+}
+
---
+++ Task015/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.WriteLine("Введите номер дня недели ");
+int day = Convert.ToInt32(Console.ReadLine());
+
+void Check(int day)
+{
+if (day == 6 || day == 7) 
+    Console.Write($"{day} -> Да");
+    else if (day < 1 || day > 7)
+    {
+        Console.Write("Нет дня с таким номером");
+    }
+    else
+    Console.Write($"{day} -> Нет");
+}
+Check(day);
