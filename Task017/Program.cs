859e0caf503b04de4d48e84689e48a158318ef3a
Yura <scsspb@mail.ru>
2022-06-30 23:34:50 +0300

    add Task 17, 18

---
+++ Task017/Program.cs
@@ -0,0 +1,38 @@
+﻿// 17. Напишите программу которая на вход принимает координаты точки (X & Y),
+// причем X != 0, Y != 0 и выдает
+//номер четверти плоскости, в которой лежит эта точка
+
+Console.WriteLine("Координаты точки X  и Y ");
+Console.Write("X: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+string Quarter (int xc, int yc)
+{
+    if (xc > 0 && yc > 0) return "Первая четверть";
+    if (xc < 0 && yc > 0) return "Вторая четверть";
+    if (xc < 0 && yc < 0) return "Третья четверть";
+    if (xc > 0 && yc < 0) return "Четвертая четверть";
+    return "Введены некорректные координаты";
+}
+string result = Quarter (x, y);
+Console.WriteLine(result);
+
+// if (x > 0 && y > 0) Console.WriteLine("Первая четверть");
+// else if (x < 0 && y > 0) Console.WriteLine("Вторая четверть");
+// else if (x < 0 && y < 0) Console.WriteLine("Третья четверть");
+// else if (x > 0 && y < 0) Console.WriteLine("Четвертая четверть");
+// else Console.WriteLine("Введены некорректные координаты");
+
+// string Quarter (int xc, int yc)
+// {
+//     string result = "";
+//     if (x > 0 && y > 0) result = "Первая четверть";
+//     else if (x < 0 && y > 0) result = "Вторая четверть";
+//     else if (x < 0 && y < 0) result = "Третья четверть";
+//     else if (x > 0 && y < 0) result = "Четвертая четверть";
+//     else result = "Введены некорректные координаты";
+//     return result;
+// }
+
---
+++ Task018/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задача 18.
+// Напишите программу, которая по заданному номеру четверти, показывает диапазон
+// возможных координат этой четверти (x и y)/
+
+Console.WriteLine("Введите номер четверти ->> ");
+int quater = Convert.ToInt32(Console.ReadLine());
+
+string Koord (int q)
+{
+    if (q == 1) return "Координаты X > 0 и Y > 0";
+    if (q == 2) return "Координаты X < 0 и Y > 0";
+    if (q == 3) return "Координаты X < 0 и Y < 0";
+    if (q == 4) return "Координаты X > 0 и Y < 0";
+    return "Введена некорректная четверть";
+}
+string result = Koord (quater);
+Console.WriteLine(result);
+
+
+// string Koord (int q)
+// {
+//     switch (q)
+//     {
+//         case 1:
+//         return "Координаты X > 0 и Y > 0";
+//         break;
+
+//         case 2:
+//         return "Координаты X < 0 и Y > 0";
+//         break;
+
+//         case 3:
+//         return "Координаты X < 0 и Y < 0";
+//         break;
+
+//         case 4:
+//         return "Координаты X > 0 и Y < 0";
+//         break;
+
+//         default:
+//         return "Введена некорректная четверть";
+//         break;
+
+//     }
+    
+    
+// }
