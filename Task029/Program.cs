765998cb1d6b8d9ba8c8a9fa16db2744cc24d8c1
Yura <scsspb@mail.ru>
2022-07-06 21:47:28 +0300

    add 29

--- Task029/Program.cs
+++ Task029/Program.cs
@@ -1,38 +1,8 @@
 ﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов, заполненный псевдослучайными числами и выводит их на экран.
 // 1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
 
-// int[] array = new int[8];
-// int len = array.Length;
-// int index = 0;
-// Random rnd = new Random();
-
-// while (index < array.Length)
-// {
-//     array[index] = rnd.Next(0, 33);
-//     index++;
-// }
-
-// for (int i = 0; i < len; i++)
-// {
-//     if (i == 0) Console.Write("[");
-//     if (i < len - 1) Console.Write(array[i] + ",");
-//     else Console.Write(array[i] + "]");
-// }
-
-int[] ArrayRandom(int size)
-{
-    int[] array = new int[8];
-    int index = 0;
-    Random rnd = new Random();
-
-    while (index < array.Length)
-    {
-        array[index] = rnd.Next(0, 33);
-        index++;
-    }
-    return array;
-}
-void PrintArray(int[] arr)
+int[] arr = {1, 2, 5, 7, 19, 6, 1, 33};
+void PrintArray(int[] array)
 {
     for (int i = 0; i < arr.Length; i++)
     {
@@ -40,6 +10,34 @@ void PrintArray(int[] arr)
             if (i <  arr.Length - 1) Console.Write(arr[i] + ",");
             else Console.Write(arr[i] + "]");
     }
+    
 }
-int[] resArray = ArrayRandom(8);
-PrintArray(resArray);
+
+PrintArray(arr);
+ 
+
+
+// int[] ArrayRandom(int size)
+// {
+//     int[] array = new int[8];
+//     int index = 0;
+//     Random rnd = new Random();
+
+//     while (index < array.Length)
+//     {
+//         array[index] = rnd.Next(0, 33);
+//         index++;
+//     }
+//     return array;
+// }
+// void PrintArray(int[] arr)
+// {
+//     for (int i = 0; i < arr.Length; i++)
+//     {
+//             if (i == 0) Console.Write("[");
+//             if (i <  arr.Length - 1) Console.Write(arr[i] + ",");
+//             else Console.Write(arr[i] + "]");
+//     }
+// }
+// int[] resArray = ArrayRandom(8);
+// PrintArray(resArray);
--- Task030/Program.cs
+++ Task030/Program.cs
@@ -16,3 +16,21 @@ int[] array = new int[8];
 var str = string.Join(" ", array);
 
 Console.Write(str);
+
+                                // int[] array = new int[8];
+                                // int len = array.Length;
+                                // int index = 0;
+                                // Random rnd = new Random();
+
+                                // while (index < array.Length)
+                                // {
+                                //     array[index] = rnd.Next(0, 33);
+                                //     index++;
+                                // }
+
+                                // for (int i = 0; i < len; i++)
+                                // {
+                                //     if (i == 0) Console.Write("[");
+                                //     if (i < len - 1) Console.Write(array[i] + ",");
+                                //     else Console.Write(array[i] + "]");
+                                //
